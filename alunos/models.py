11cfe099f8ef4fa0a63fad54c0f20d8ec0b696ca
renato_chagas <renatochagas.m@gmail.com>
2025-02-03 11:12:44 -0300

    feat: Estados

--- alunos/models.py
+++ alunos/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class Estado(models.Model):
+    nome = models.CharField(max_length=50)
+    sigla = models.CharField(max_length=2)
+
+    def __str__(self):
+        return self.nome
